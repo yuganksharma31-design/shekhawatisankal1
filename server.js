cc9bad963446b84fe4afb7d4283573fac6187b4d
yuganksharma31-design <yuganksharma31@gmail.com>
2026-04-28 17:43:21 +0000

    final deploy

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ const path = require("path")
 
 const app = express()
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
