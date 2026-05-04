16dcd9b380f3a34bd0bad04d0024ba45b52d0842
yuganksharma31-design <yuganksharma31@gmail.com>
2026-05-04 09:31:38 +0000

    updated gnews api for Rajasthan Hindi news

--- api/fetch-news.js
+++ api/fetch-news.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   try {
     const apiKey = "607a770709491bc910990f3ea1fd2f39";
 
-    const url = `https://gnews.io/api/v4/search?q=rajasthan&lang=hi&token=${apiKey}`;
+    const url = `https://gnews.io/api/v4/search?q=rajasthan OR jaipur OR jodhpur OR udaipur OR kota OR Jhunjhunu &lang=hi&country=in&max=10&token=${apiKey}`;
 
     const response = await fetch(url);
     const data = await response.json();
